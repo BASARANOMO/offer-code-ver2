08e6b510473452492638f3a1da6c2a367eccc18d
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-20 14:00:31 +0100

    offer 04

---
+++ solutions/剑指 Offer 04. 二维数组中的查找 LCOF/README.md
@@ -0,0 +1,7 @@
+# 剑指 Offer 04. 二维数组中的查找 LCOF
+[LeetCode-cn链接](https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/)
+
+## 解法1
+Binary search
+
+## 解法2
---
+++ solutions/剑指 Offer 04. 二维数组中的查找 LCOF/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+        for row in matrix:
+            if (row[0] > target) or (row[-1] < target):
+                continue
+            else: # binary search
+                if self.bin_search(row, target):
+                    return True
+        return False
+
+    def bin_search(self, nums: List[int], target: int) -> bool:
+        front, end = 0, len(nums) - 1
+        while front <= end:
+            mid = (front + end) // 2
+            if nums[mid] < target:
+                front = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                return True
+        return False
