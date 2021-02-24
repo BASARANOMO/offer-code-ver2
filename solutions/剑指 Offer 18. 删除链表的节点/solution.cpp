d131097a2f1f2abc2f3b28b4b3c1211da787864b
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-24 14:53:11 +0800

    offer 18

---
+++ solutions/剑指 Offer 18. 删除链表的节点/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteNode(ListNode* head, int val) {
+        if (!head) return head;
+        if (head->val == val) return head->next;
+        ListNode* curr = head;
+        while (curr->next) {
+            if (curr->next->val == val) {
+                curr->next = curr->next->next;
+                break;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
